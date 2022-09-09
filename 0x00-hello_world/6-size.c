761072dc52d8ee06c61474d5951b092bc3923845
Eleja99 <elejaabdqadr@gmail.com>
2022-09-08 18:23:19 -0700

    using the sizeof function

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%c\n", sizeof(char));
+	printf("%d\n", sizeof(int));
+	printf("%ld\n", sizeof(long int));
+	printf("%lld\n", sizeof(long long int));
+	printf("%f\n", sizeof(float));
+	return (0);
+}
