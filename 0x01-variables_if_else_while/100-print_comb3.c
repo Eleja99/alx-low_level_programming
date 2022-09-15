b5d8e027a7daf07ea65dde64e3a3d6e61103e261
Eleja99 <elejaabdqadr@gmail.com>
2022-09-15 04:36:09 -0700

    a program that prints all possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is postive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 100; c++)
+	{
+		putchar((c / 10) + '0' );
+		putchar((c % 10) + '0' );
+	
+	if (c != 99)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
